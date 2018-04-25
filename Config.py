8f5a9b28e235d187c80eca0c6a9d99206549e52b
Ramy <ramy.shelbaya@gmail.com>
2018-04-26 00:41:58 +0100

    Created main file. Created parameters class that stores the variables dictionary. Creating plotting module. Python already flags an exception when given the name of a file that it cannot find so no need to manually add it.

---
+++ Config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 25 23:24:21 2018
+
+@author: Rami
+"""
+
+class parameters() :
+    
+    def __init__(self) :
+        self.variables = {'xlimits': [0,80],
+                          'ylimits': [0,80]}
+
+
+        
