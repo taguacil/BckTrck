6145d9a0478b36764ce2148be4ce8aea8293a0e4
Taimir Aguacil <aguaciltaimir@gmail.com>
2018-04-30 22:26:53 +0200

    Some minor modifications, but current version will not run due to incomplete Random_walker and incompatible arguments with main

--- Navigation/Random_walker.py
+++ Navigation/Random_walker.py
@@ -1,34 +1,58 @@
-# -*- coding: utf-8 -*-
+# %-*- coding: utf-8 -*-
 """
-Created on Thu Apr 26 06:09:24 2018
+ =============================================================================
+ Title       : Random walker
+ Project     : Simulation environment for BckTrk app
+ File        : Random_walker.py
+ -----------------------------------------------------------------------------
 
-@author: Rami
+   Description :
+
+   This file generates a random 2d walk over a grid, can only move in cardinal directions, 
+   the stepsize in normalized to 1 to avoid numerical precision problems, 
+   returns a 2d array with the x-positions on one axis and the y-positions on the other 
+   
+   References :
+
+   -
+ -----------------------------------------------------------------------------
+ Revisions   :
+   Date         Version  Name      Description
+   26-Apr-2018  1.0      Rami      File created
+ =============================================================================
+ 
 """
-
+## Python library import
 import numpy as np 
 
-def random_2d_path_generator(params):
-    """ generates a random 2d walk over a grid, can only move in cardinal directions, the stepsize in normalized to 1 to avoid numerical precision problems, returns a 2d array with the x-positions on one axis and the y-positions on the other """
-    
-    number_of_steps = params['number_of_steps']
-    random_seed = params['random_seed']
-    use_random_seed = params['use_random_seed'] # Controls whether we want to use our random seed or let numpy initialize the randomstate "randomly"
-    
-    if use_random_seed :
-        np.random.seed(random_seed)
-    
-    positions = np.zeros((2,number_of_steps))
-    
-    for i in range(1,number_of_steps) :
-        chance = np.random.randint(0,4)
-        if chance == 0 :
-            positions[:,i] = positions[:,i-1] + np.array([1,0])
-        if chance == 1 : 
-            positions[:,i] = positions[:,i-1] + np.array([0,1])
-        if chance == 2 :
-            positions[:,i] = positions[:,i-1] + np.array([-1,0])
-        if chance == 3 :
-            positions[:,i] = positions[:,i-1] + np.array([0,-1])
-        
-    return positions 
+## Bring the logger
+import logging
+logger = logging.getLogger(__name__)
 
+## the main processing function 
+def random_2d_path_generator (params):
+    data_obj = cRandomPath (params)
+    
+class cRandomPath:
+    ## Constructor
+    def __init__(self,struct):
+        self.m_localStruct = struct
+        # Controls whether we want to use our random seed or let numpy initialize the randomstate "randomly"
+        self.m_use_random_seed = struct['use_random_seed']
+        self.m_random_seed = struct['random_seed']
+        self.m_acquisition_length = struct['gps_freq_Hz'] * struct['acquisition_time_sec']
+        self.m_positions = np.zeros ((2,self.m_aquisition_length))
+        if self.m_use_random_seed :
+            np.random.seed(self.m_random_seed)
+            
+    ## An example of required methods    
+    
+    ## Set randomly initial position (longitute and lattitude)
+    
+    ## Generate randomly the bearings(radian) and distances (meter)
+    
+    ## Compute new position with equations
+    
+    ## Calculate distance and bearing between 2 positions (optional, might be helpful later)
+    
+        
