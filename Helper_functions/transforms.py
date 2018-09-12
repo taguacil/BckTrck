74518967b9bde0ccd2877db9a7fbdcc9ab109de4
Taimir Aguacil <taimiraguacil15@hotmail.com>
2018-09-12 19:03:21 +0200

    Code cleaning and PSD implementation

--- Helper_functions/transforms.py
+++ Helper_functions/transforms.py
@@ -19,32 +19,35 @@
  =============================================================================
  
 """
-## Python library import
-import numpy as np 
+# Python library import
+import numpy as np
 import scipy.fftpack as ft
 
 ## Bring the logger
 import logging
+
 logger = logging.getLogger('BckTrk')
 
-## the main processing function 
-def transforms (params,paths_latlon_noisy):
-    data_obj = cTransforms (params,paths_latlon_noisy)
+
+# the main processing function
+def transforms(params, paths_latlon_noisy):
+    data_obj = cTransforms(params, paths_latlon_noisy)
     if params['TRANSFORM']['bDctTransform']:
-        return(data_obj.transform_dct())
-    
+        return (data_obj.transform_dct())
+
+
 class cTransforms:
-    ## Constructor
-    def __init__(self,struct,paths_latlon_noisy):
-        self.m_latlonpath           = paths_latlon_noisy
-        self.m_acquisition_length   = struct['gps_freq_Hz']*struct['acquisition_time_sec']
-        
-    ## Write dictionary into pickle format to txt file    
-    def transform_dct(self) :
+    # Constructor
+    def __init__(self, struct, paths_latlon_noisy):
+        self.m_latlonpath = paths_latlon_noisy
+        self.m_acquisition_length = struct['gps_freq_Hz'] * struct['acquisition_time_sec']
+
+    # Write dictionary into pickle format to txt file
+    def transform_dct(self):
         logger.debug("DCT transform")
-        
-        transformed_path        = np.zeros((2,self.m_acquisition_length))
-        transformed_path[0,:]   = ft.dct(self.m_latlonpath[0,:])
-        transformed_path[1,:]   = ft.dct(self.m_latlonpath[1,:])
-        
+
+        transformed_path = np.zeros((2, self.m_acquisition_length))
+        transformed_path[0, :] = ft.dct(self.m_latlonpath[0, :])
+        transformed_path[1, :] = ft.dct(self.m_latlonpath[1, :])
+
         return transformed_path
